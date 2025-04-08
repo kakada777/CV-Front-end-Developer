4fe01010530c1a3bcd03ee0ed7846ea63a8a4347
macbookpro <kakakda772003@gmail.com>
2025-04-08 12:25:01 +0700

    first commit

---
+++ javascript/index.js
@@ -0,0 +1,34 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const scrollBtn = document.getElementById("scrollBtn");
+    
+    window.addEventListener("scroll", function() {
+        if (window.scrollY > 100) {
+            scrollBtn.classList.add("show");
+        } else {
+            scrollBtn.classList.remove("show");
+        }
+    });
+    
+    scrollBtn.addEventListener("click", function() {
+        window.scrollTo({ top: 0, behavior: "smooth" });
+    });
+});
+/*Contact */
+document.getElementById("contact-form").addEventListener("submit", function (e) {
+    e.preventDefault();
+  
+    emailjs.send("service_kimbv9s", "template_t29p5tb", {
+      user_email: document.getElementById("user_email").value,
+      subject: document.getElementById("subject").value,
+      message: document.getElementById("message").value,
+    })
+    .then(function(response) {
+      alert("Message sent successfully!");
+      document.getElementById("contact-form").reset();
+    }, function(error) {
+      alert("Failed to send message. Please try again.");
+      console.error("FAILED...", error);
+    });
+  });
+  
+  
